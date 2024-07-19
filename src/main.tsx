e52361d4bea6485a57264a4442697b14fd8996f3
Kennybabs-py <babalolakehinde74@gmail.com>
2024-07-19 14:37:56 +0100

    added carousel slides

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import gsap from "gsap";
+import Draggable from "gsap/Draggable";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+gsap.registerPlugin(Draggable);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,8 +1,12 @@
-import { useState } from "react";
+import { useState, useRef, createRef } from "react";
 import { IoMailOutline } from "react-icons/io5";
 import { FaPhoneAlt } from "react-icons/fa";
 import { RxHamburgerMenu } from "react-icons/rx";
 import { IoMdClose } from "react-icons/io";
+import { gsap } from "gsap";
+import { useGSAP } from "@gsap/react";
+
+import imagedata from "@/lib/data.json";
 
 import MardentLogo from "@/assets/mardent-energy.png";
 import "./home.scss";
@@ -10,6 +14,115 @@ import { Link } from "react-router-dom";
 
 export default function Home() {
   const [open, setOpen] = useState(false);
+
+  const slidesRef = useRef(imagedata.map(() => createRef<HTMLDivElement>()));
+  const slideContainerRef = useRef<HTMLDivElement | null>(null);
+  const sectionRef = useRef<HTMLElement | null>(null);
+
+  useGSAP(
+    (_, contextSafe) => {
+      if (!contextSafe) return;
+      if (!sectionRef.current) return;
+      if (!slideContainerRef.current || slideContainerRef.current === null)
+        return;
+      if (!slidesRef.current) return;
+      const single_slide = slidesRef.current[0];
+      if (!single_slide.current) return;
+      // const slides =
+      //   slideContainerRef?.current.querySelectorAll(".slide__item");
+      const slideDelay = 6;
+      const slideDuration = 3;
+      const wrap = true;
+      const slides = document.querySelectorAll(".slide__item");
+      const progressWrap = gsap.utils.wrap(0, 1);
+      const numSlides = slides.length;
+
+      gsap.set(slides, {
+        // backgroundColor:
+        //   "random([red, blue, green, purple, orange, yellow, lime, pink])",
+        xPercent: (i) => i * 100,
+      });
+
+      const wrapX = gsap.utils.wrap(-100, (numSlides - 1) * 100);
+      const timer = gsap.delayedCall(slideDelay, autoPlay);
+      const animation = gsap.to(slides, {
+        xPercent: "+=" + numSlides * 100,
+        duration: 1,
+        ease: "none",
+        paused: true,
+        repeat: -1,
+        modifiers: {
+          xPercent: wrapX,
+        },
+      });
+      const proxy = document.createElement("div");
+      let slideAnimation = gsap.to({}, {});
+      let slideWidth = 0;
+      let wrapWidth = 0;
+
+      function animateSlides(direction: number) {
+        timer.restart(true);
+        slideAnimation.kill();
+        const x = Number(
+          snapX(Number(gsap.getProperty(proxy, "x"))) + direction * slideWidth
+        );
+        slideAnimation = gsap.to(proxy, {
+          x: x,
+          duration: slideDuration,
+          onUpdate: updateProgress,
+        });
+      }
+      function autoPlay() {
+        // if (
+        //   draggable.isPressed ||
+        //   draggable.isDragging ||
+        //   draggable.isThrowing
+        // ) {
+        //   timer.restart(true);
+        // } else {
+        //   animateSlides(-1);
+        // }
+        animateSlides(-1);
+        // timer.restart(true);
+      }
+      function updateProgress() {
+        animation.progress(
+          progressWrap(Number(gsap.getProperty(proxy, "x")) / wrapWidth)
+        );
+      }
+
+      function snapX(value: number) {
+        const snapped = gsap.utils.snap(slideWidth, value);
+        return wrap
+          ? snapped
+          : gsap.utils.clamp(-slideWidth * (numSlides - 1), 0, snapped);
+      }
+      function resize() {
+        const norm = Number(gsap.getProperty(proxy, "x")) / wrapWidth || 0;
+        slideWidth = single_slide.current?.offsetWidth || 0;
+        wrapWidth = slideWidth * numSlides;
+        // wrap ||
+        //   draggable.applyBounds({
+        //     minX: -slideWidth * (numSlides - 1),
+        //     maxX: 0,
+        //   });
+
+        gsap.set(proxy, {
+          x: norm * wrapWidth,
+        });
+        animateSlides(0);
+        slideAnimation.progress(1);
+      }
+      resize();
+      window.addEventListener("resize", resize);
+
+      return () => {
+        window.removeEventListener("resize", resize);
+      };
+    },
+    { scope: sectionRef }
+  );
+
   return (
     <main>
       <header>
@@ -88,7 +201,22 @@ export default function Home() {
         </nav>
       </header>
 
-      <section className="hero">
+      <section className="hero" ref={sectionRef}>
+        <div className="slides__wrapper">
+          <div className="slides__inner__wrapper" ref={slideContainerRef}>
+            {imagedata.map((item, index) => (
+              <div
+                key={index}
+                className="slide__item"
+                ref={slidesRef.current[index]}
+              >
+                <figure>
+                  <img src={item.image} alt="image" />
+                </figure>
+              </div>
+            ))}
+          </div>
+        </div>
         {/* <h3>Unlocking Potential, Driving Growth</h3>
         <h2>Powering Progress with Indigenous Excellence</h2>
         <p>
